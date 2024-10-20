3a24b51ac0b0e78eeb80972221b54d77aefafb97
Ihita-123 <ihitab.27@gmail.com>
2024-10-20 19:02:42 +0200

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,15 @@
+Countries=("Japan", "Australia", "India", "France", "Germany", "Poland")
+
+print(type(Countries))
+
+print(len(Countries))
+
+print(Countries[::-1])
+
+print(Countries[3])
+
+for i in Countries:
+    print(i)
+
+del Countries
+print(Countries)
